49c4574c59269a532377c43a3720fa8c0b67a551
johnmolin91 <john.molin91@gmail.com>
2018-02-09 16:15:25 -0500

    turnin

--- app/routes/apiRoutes.js
+++ app/routes/apiRoutes.js
@@ -3,11 +3,22 @@ var friendData = require("../data/friends.js");
 module.exports = function(app) {
 
 	app.get("/api/friends", function(req, res) {
-		res.json(friends);
+		res.json(friendData);
 	});
 
 	app.post("/api/friends", function(req, res) {
-		friendData.push()
+		friendData.push(req.body);
+		res.json(true);
+
+		var bestMatch = {
+			name: "",
+			friendDifference: 1000
+		};
+
+		var userData = req.body;
+		var userScores = userData.scores;
+
+		console.log(userScores);
 	})
 
 }
